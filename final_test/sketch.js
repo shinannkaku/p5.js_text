b27f5646c529caeefbb348037f5c72ca50e1bfd3
shinannkaku <shinannkaku@gmail.com>
2023-12-28 21:12:46 +0800

    1228 雪花 極光

--- final_test/sketch.js
+++ final_test/sketch.js
@@ -1,38 +1,14 @@
 function setup() {
     // put setup code here
     createCanvas(1200, 650);
-    background('white');
+    background('black');
     drawChristmasTree();
 }
-    /*function draw() {
-        // put drawing code here
-        stroke(255);
-        s = random(1, 10);
-        x = random(0, width);
-        y = random(0, height);
-        strokeWeight(s);
-        PointerEvent(x, y);
-    }
-
-    function draw() {
-        // put drawing code here
-        r = random(300);
-        g = random(255);
-        b = random(255);
-        strokeColor = color(r, g, b);
-        //x1 = random(0, width/2);
-        //y1 = random(height/2, height);
-        x1 = width/5; //修改線條長度
-        y1 = height/5;
-        x2 = random(0, width-1);
-        y2 = random(0, height-1);
-        stroke(strokeColor);
-        line(x1, y1, x2, y2); //線條粗細
-        strokeWeight(random);    
-    }*/
       
       function draw() {
         // Optional: you can leave this blank as all drawing is done in setup
+        drawline();
+        //drawSnow(); //呼叫繪製雪花的函數
       }
       
       function drawChristmasTree() {
@@ -49,16 +25,45 @@ function setup() {
         /*(x1,y1):三角形頂點 x在畫布的中心 y是計算得到的等腰三角形頂點的y座標
         (x2,y2): 右下 x在畫布中心向右偏移60像素 y於畫布底部80像素
         (x3,y3): 左下 x在畫布的中心像左偏移60像素 y於畫布底部80像素*/
-      
+
         // Draw ornaments (circles)
         const numOrnaments = 50; //定義紅圈的數量
         for (let i = 0; i < numOrnaments; i++) {
           const x = random(0, width); //寬度在畫布的範圍內
           const y = random(120, height - 80); //(120:y座標最小值,確保裝飾品不會在樹幹下)
           const radius = random(3, 9); //隨機半徑
-          fill('red'); //圓圈填充色
+          fill(random(255), random(255), random(255)); //圓圈填充色
           noStroke(); //圓圈不要有邊框
-          ellipse(x, y, radius * 2); //
+          ellipse(x, y, radius * 2); //繪製圓圈
+        }
+      
+
+        // Draw Circles(snow)
+        const numCircles = 30; //定義白圈(雪)的數量
+        for (let i = 0; i < numCircles; i++) {
+          const x = random (0, width); //寬度在畫布的範圍內
+          const y = random(120, height);
+          const radius = random(10, 12);
+          fill('white');
+          stroke('white');
+          ellipse(x, y, radius * 2);
         }
       }
-      
+     
+
+      function drawline() { 
+        r = random(255); 
+        g = random(255);
+        b = random(255);
+        strokeColor = color(r, g, b);  //rgb隨機的顏色
+        //x1 = random(0, width/2);
+        //y1 = random(height/2, height);
+        x1 = width/5; //固定起點X座標
+        y1 = height/5; //固定起點Y座標
+        x2 = random(0, width-1); //終點X座標(width-1:畫布最右邊的點/畫布的寬:0~widht-1)
+        y2 = random(0, height-1); //終點Y座標(height-1:畫布最下方的點/height:超出畫布的範圍)
+        stroke(strokeColor); //line55
+        line(x1, y1, x2, y2); //線條起點+終點座標
+        strokeWeight(random);  //線條粗細  
+      }
+
